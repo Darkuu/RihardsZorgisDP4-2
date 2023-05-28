65e656fd316c19a0316d128604f84049dbd09651
Rihards Zorgis <Daarkuwu@gmail.com>
2023-05-28 15:24:49 +0300

    Clearing old files

--- templates/exit.php
+++
@@ -1,7 +0,0 @@
-<?php
-    setcookie('user', $user['Epasts'], time() - 3600, "/");
-    header('Location: /schoolpage/index.html');
-
-     // Stops the cookie from working, by removing its time limit
-?>
-
